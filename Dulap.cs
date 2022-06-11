8ab0cb120f78106045b31f0725d600c46040bf0b
marius.herciu23 <marius.herciu23@gmail.com>
2022-06-11 14:41:38 +0300

    Add project files.

---
+++ Dulap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1_Laborator6_
+{
+    class Dulap
+    {
+        private int lungime;
+        private int latime;
+        private int inaltime;
+
+        public Dulap(int lungime, int latime, int inaltime)
+        {
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+        }
+        public int GetCalculVolum()
+        {
+            int volum = lungime * latime * inaltime;
+            return volum;
+        }
+        public void TiparesteVolum()
+        {
+            Console.WriteLine("Dulapul are volumul " + GetCalculVolum());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ex1_Laborator6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dulap dulap = new Dulap(5, 4, 2);
+            dulap.TiparesteVolum();
+        }
+    }
+}
